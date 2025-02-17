99d9ac3641594238b51bf9a913b6c30d79a5936a
tarungajamoni <saitarun.goud1998@gmail.com>
2025-02-17 22:09:02 +0530

    UI: login, register CSS

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import api from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const router = useNavigate();
+
   const handleClick = async () => {
     try {
       const response = await api.post("/auth/login", { username, password });
@@ -13,20 +16,42 @@ const Login = () => {
       alert("Login failed");
     }
   };
+  
   return (
-    <>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleClick}>Login</button>
-    </>
+    <div className="h-screen flex items-center justify-center bg-slate-50 ">
+      <div className="bg-green-100 p-10 rounded-lg flex flex-col gap-2">
+        <div className="flex flex-col ">
+          <span className="text-left">Username</span>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="h-8 border border-gray-300 bg-gray-50 rounded-md px-2"
+            placeholder="Enter Username"
+          />
+        </div>
+        <div className="flex flex-col ">
+          <span className="text-left">Password</span>
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="h-8 border border-gray-300 bg-gray-50 rounded-md px-2"
+            placeholder="Enter Password"
+          />
+        </div>
+        <button
+          onClick={handleClick}
+          className=" border border-gray-500 px-2 py-1 bg-violet-300 rounded-lg my-4 text-white hover:text-violet-700 hover:bg-white">
+          Log In
+        </button>
+        <span
+          className="underline cursor-pointer text-gray-600"
+          onClick={() => router("/register")}>
+          New User? Click here to Register
+        </span>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,37 +1,72 @@
 import React, { useState } from "react";
 import api from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
+
+  const router = useNavigate();
+
   const handleClick = async () => {
     try {
-      const response = await api.post("/auth/register", { username, email, password });
+      const response = await api.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
       if (response) alert("registration successful");
     } catch (e) {
       alert("registration failed");
     }
   };
+  
   return (
-    <>
-    <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleClick}>Register</button>
-    </>
+    <div className="h-screen flex items-center justify-center bg-slate-50 ">
+      <div className="bg-green-100 p-10 rounded-lg flex flex-col gap-2">
+        <div className="flex flex-col ">
+          <span className="text-left">Username</span>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="h-8 border border-gray-300 bg-gray-50 rounded-md px-2"
+            placeholder="Enter Username"
+          />
+        </div>
+        <div className="flex flex-col ">
+          <span className="text-left">Username</span>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="h-8 border border-gray-300 bg-gray-50 rounded-md px-2"
+            placeholder="Enter Email"
+          />
+        </div>
+        <div className="flex flex-col ">
+          <span className="text-left">Password</span>
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="h-8 border border-gray-300 bg-gray-50 rounded-md px-2"
+            placeholder="Enter Password"
+          />
+        </div>
+        <button
+          onClick={handleClick}
+          className=" border border-gray-500 px-2 py-1 bg-violet-300 rounded-lg my-4 text-white hover:text-violet-700 hover:bg-white">
+          Register
+        </button>
+        <span
+          className="underline cursor-pointer text-gray-600"
+          onClick={() => router("/login")}>
+          Already a User? Click here to Login
+        </span>
+      </div>
+    </div>
   );
 };
 
